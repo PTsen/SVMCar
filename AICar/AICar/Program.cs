e33392ab537fdc141bc9af69ed540cc2ebfc4ae2
ptsen <ping.sen@hotmail.com>
2019-03-29 15:59:40 +0100

    Initialization

---
+++ AICar/AICar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AICar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello World!");
+        }
+    }
+}
